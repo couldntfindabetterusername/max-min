f2d719e8583073deffb661ea4fe0d1ed218a3b4a
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-26 14:04:12 +0530

    arrays used

--- src/App.java
+++ src/App.java
@@ -2,14 +2,14 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         FindMax<Integer> intObj = new FindMax<Integer>(20, 15, 25);
-        System.out.println("Max of " + intObj.val1 + ", " + intObj.val2 + ", " + intObj.val3 + ": " + intObj.getMax());
+        System.out.println("Max of given values: " + intObj.getMax());
 
         FindMax<Float> floatObj = new FindMax<Float>(20.2F, 20.3F, 20.1F);
         System.out.println(
-                "Max of " + floatObj.val1 + ", " + floatObj.val2 + ", " + floatObj.val3 + ": " + floatObj.getMax());
+                "Max of given values: " + floatObj.getMax());
 
         FindMax<String> stringObj = new FindMax<String>("Apple", "Banana", "Peach");
         System.out.println(
-                "Max of " + stringObj.val1 + ", " + stringObj.val2 + ", " + stringObj.val3 + ": " + stringObj.getMax());
+                "Max of given values: " + stringObj.getMax());
     }
 }
--- src/FindMax.java
+++ src/FindMax.java
@@ -1,29 +1,23 @@
-public class FindMax<T extends Comparable<T>> {
-    public T val1;
-    public T val2;
-    public T val3;
+import java.util.Arrays;
 
-    FindMax(T val1, T val2, T val3) {
-        this.val1 = val1;
-        this.val2 = val2;
-        this.val3 = val3;
+public class FindMax<T extends Comparable<T>> {
+    public T[] values;
+
+    FindMax(T... values) {
+        this.values = values;
     }
 
     public T getMax() {
-        return FindMax.getMax(val1, val2, val3);
+        return FindMax.getMax(values);
     }
 
-    public static <T extends Comparable<T>> T getMax(T val1, T val2, T val3) {
-        T max = val1;
-
-        if (val2.compareTo(max) > 0) {
-            max = val2;
+    public static <T extends Comparable<T>> T getMax(T... values) {
+       if (values == null || values.length == 0) {
+        // System.out.println();
+        return null;
         }
 
-        if (val3.compareTo(max) > 0) {
-            max = val3;
-        }
-
-        return max;
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 }
